1a11817a50940d2d4bef884ccf9564795d5de395
yujeiry <43036901+yujeiry@users.noreply.github.com>
2019-03-28 19:01:26 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+$(".white").click(function() {
+  $(".black").slideToggle();
+});
+
+$(".kitty").click(function() {
+  $(".crazykitty").slideToggle();
+});
+
+$(".puppy").click(function() {
+  $(".locopuppy").slideToggle();
+});
+
+
+
+
+
